dd9267f67f363a603a22f23c780e23d664d9e685
IWPTeam <iwebpteam@gmail.com>
2021-07-20 15:04:20 +0300

    auth design completed

--- app/src/main/java/com/lfx/shopuna/RegistrationActivity.kt
+++ app/src/main/java/com/lfx/shopuna/RegistrationActivity.kt
@@ -2,10 +2,14 @@ package com.lfx.shopuna
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
     }
+
+    fun goAuthActivity(view: View) {}
+    fun register(view: View) {}
 }
